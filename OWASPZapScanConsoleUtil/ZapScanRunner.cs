b073b130c79045c8903eabe24c3d3b413334fce1
HEALTHTECHINDIA\KrishnrajR <krishnrajr@healthtechindia.com>
2022-11-11 13:01:40 -0500

    Initial commit of Zap Scan Console application

--- OWASPZapScanConsoleUtil/ZapScanRunner.cs
+++ OWASPZapScanConsoleUtil/ZapScanRunner.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Diagnostics;
 using System.IO;
-using System.Text;
 using Microsoft.Extensions.Configuration;
 using Serilog;
+using CliWrap;
+using CliWrap.Buffered;
 
 namespace OWASPZapScanConsoleUtil
 {
@@ -14,51 +14,77 @@ namespace OWASPZapScanConsoleUtil
 
         public static IConfiguration Configuration { get; set; }
 
+        public static async void StartScan()
+        {
+            ValidateZapInstallation();
+
+            Console.WriteLine("Site scan started..");
+            _logger.Information("Site scan started at - {Now}", DateTime.Now.ToString("f"));
+
+            var commandResult = await Cli.Wrap("powershell.exe")
+                .WithWorkingDirectory(Configuration["ZAPScan:ZapInstallPath"])
+                .WithArguments(new[]
+                    {
+                        "java",
+                        "-jar",
+                        "zap-2.12.0.jar",
+                        "-cmd",
+                        "-quickurl",
+                        Configuration.GetValue<string>("ZAPScan:WebsiteUrlToScan"),
+                        "-quickprogress",
+                        "-quickout",
+                        $"{Path.Combine(Configuration.GetValue<string>("ZAPScan:ReportPath"), $"Report_{DateTime.Now:yyyy_MM_dd_hh_mm}.html")}"
+                    }
+                )
+                .ExecuteBufferedAsync();
+
+            _logger.Information(commandResult.StandardOutput);
+        }
+
         /// <summary>
         /// Get CLI Command of ZAP which we'll run in Powershell CLI
         /// </summary>
         /// <returns></returns>
-        private static string GetZapCommand()
-        {
-            string resultFile = $"Report_{DateTime.Now:yyyy_MM_dd_hh_mm}.html";
-            
-            StringBuilder commandBuilder = new();
-            commandBuilder.Append("java -jar zap-2.12.0.jar -cmd ");
-            commandBuilder.Append($"-quickurl {Configuration.GetValue<string>("ZAPScan:WebsiteUrlToScan")} ");
-            commandBuilder.Append("-quickprogress ");
-            commandBuilder.Append($"-quickout {Path.Combine(Configuration["ZAPScan:ReportPath"], resultFile)}");
-            return commandBuilder.ToString();
-        }
+        //private static string GetZapCommand()
+        //{
+        //    string resultFile = $"Report_{DateTime.Now:yyyy_MM_dd_hh_mm}.html";
 
-        public static void StartScan()
-        {
-            ValidateZapInstallation();
-            string scanCommand = GetZapCommand();
-            
-            Process process = new()
-            {
-                StartInfo = new()
-                {
-                    FileName = "powershell.exe",
-                    CreateNoWindow = false,
-                    UseShellExecute = false,
-                    RedirectStandardInput = true,
-                    RedirectStandardOutput = true,
-                    //Arguments = @"/c cd C:\Program Files\OWASP\Zed Attack Proxy"
-                }
-            };
+        //    StringBuilder commandBuilder = new();
+        //    commandBuilder.Append("java -jar zap-2.12.0.jar -cmd ");
+        //    commandBuilder.Append($"-quickurl {Configuration.GetValue<string>("ZAPScan:WebsiteUrlToScan")} ");
+        //    commandBuilder.Append("-quickprogress ");
+        //    commandBuilder.Append($"-quickout {Path.Combine(Configuration["ZAPScan:ReportPath"], resultFile)}");
+        //    return commandBuilder.ToString();
+        //}
 
-            process.Start();
-            Console.WriteLine("Site scan started..");
-            _logger.Information("Site scan started at - {Now}", DateTime.Now.ToString("f"));
-            process.StandardInput.WriteLine($"cd \"{Configuration["ZAPScan:ZapInstallPath"]}\" ");
-            System.Threading.Tasks.Task.Delay(900);
-            process.StandardInput.WriteLine(scanCommand);
-            process.StandardInput.Flush();
-            process.StandardInput.Close();
-            _logger.Information(process.StandardOutput.ReadToEnd());
-            process.WaitForExit();
-        }
+        //public static void StartScan()
+        //{
+        //    ValidateZapInstallation();
+        //    string scanCommand = GetZapCommand();
+
+        //    Process process = new()
+        //    {
+        //        StartInfo = new()
+        //        {
+        //            FileName = "powershell.exe",
+        //            CreateNoWindow = false,
+        //            UseShellExecute = false,
+        //            RedirectStandardInput = true,
+        //            RedirectStandardOutput = true,
+        //            //Arguments = @"/c cd C:\Program Files\OWASP\Zed Attack Proxy"
+        //        }
+        //    };
+
+        //    process.Start();
+        //    Console.WriteLine("Site scan started..");
+        //    _logger.Information("Site scan started at - {Now}", DateTime.Now.ToString("f"));
+        //    process.StandardInput.WriteLine($"cd \"{Configuration["ZAPScan:ZapInstallPath"]}\" ");
+        //    process.StandardInput.WriteLine(scanCommand);
+        //    process.StandardInput.Flush();
+        //    process.StandardInput.Close();
+        //    _logger.Information(process.StandardOutput.ReadToEnd());
+        //    process.WaitForExit();
+        //}
 
         /// <summary>
         /// Throws an exception if ZAP_2_12_0 is not installed on the machine.
